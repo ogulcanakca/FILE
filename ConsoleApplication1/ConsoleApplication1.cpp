3798a840ce4a2dac81912e619f4df238f8ef5739
ogulcanakca <ogulcanakca382382@hotmail.com>
2021-12-04 16:43:38 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,36 @@
+﻿#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include <stdlib.h>
+int main(void)
+{
+    FILE* fp;
+    fpos_t pos;
+    const char* p = "Bilgisayar";
+    int id;
+
+    if ((fp = fopen("dosya.txt", "w+")) == NULL) {
+        printf("Dosya açma hatası!");
+        exit(1);
+    }
+
+    while (*p) {
+        fgetpos(fp, &pos);
+        printf("Dosyanın aktif konumu: %ld", pos);        
+        fputc(*p, fp);
+
+        printf(" karakter: %c\n", *p);
+        p++;
+    }
+    printf("\n");
+
+    rewind(fp);
+
+    while ((id = fgetc(fp)) != EOF) { /* Dosya sonuna kadar (EOF) karakter karakter okuma işlemi */
+        printf("%c", id);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
